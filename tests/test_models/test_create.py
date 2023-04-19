c9bff9620ccd77d90b1bb5372bb386d59a1b8730
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2023-04-19 22:24:38 +0000

    updated codes

--- tests/test_models/test_create.py
+++ tests/test_models/test_create.py
@@ -11,7 +11,7 @@ class TestCreateStatae(unittest.TestCase):
     This module tests the behavioural functionality of the create method
     """
 
-    def SetUp(self):
+    def setUp(self):
         connDB = MySQLdb.connect(
             user=hbnb_test,
             passwd=hbnb_test_pwd,
@@ -21,22 +21,19 @@ class TestCreateStatae(unittest.TestCase):
         self.cursor = self.connDB.cursor()
 
     def test_create_state(self):
-        self.cursor.execute("SELECT COUNT(State) \
-                FROM states")
+        self.cursor.execute("SELECT COUNT(*) FROM states")
         initialData = cursor.fetchone()[0]
         # executing the console command 'create'
-        create State name = "California"
+        "create State name = 'California'"
         # counting the number of rows in the state after addition of
         # California
-        self.cursor.execute("SELECT COUNT(State) \
-                FROM states")
+        self.cursor.execute("SELECT COUNT(*) FROM states")
         finalData = cursor.fetchone()[0]
         # testing the difference in the before and after execution of the code
         # to be 1
         self.assertEqual(finalData - initialData, 1)
 
     def tearDown(self):
-        self.cursor.execute("DELETE FROM states \
-                WHERE name='California'")
+        self.cursor.execute("DELETE FROM states WHERE name='California'")
         self.cursor.close()
         self.db.close()
