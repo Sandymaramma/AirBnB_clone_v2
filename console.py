8845ed8108014f4712eaa476e1741d65df4405af
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2023-04-20 01:24:00 +0000

    updated codes

--- console.py
+++ console.py
@@ -73,7 +73,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
+                    if pline[0] is '{' and pline[-1] is '}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -117,7 +117,7 @@ class HBNBCommand(cmd.Cmd):
         """ Create an object of any class"""
         args = arg.split()
         if len(args) < 2:
-            print("Usage: create <Class name> <param1> <param2> ...)")
+            print("Usage: create <Class name> <param1> <param2> ...")
             return
         class_name = args[0]
         if class_name not in HBNBCommand.classes:
@@ -347,6 +347,7 @@ class HBNBCommand(cmd.Cmd):
         """ Help information for the update class """
         print("Updates an object with new information")
         print("Usage: update <className> <id> <attName> <attVal>\n")
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
