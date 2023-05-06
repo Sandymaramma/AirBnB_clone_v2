4795299e4689e4f14258d6acbbc9a6199bd1e0d2
sandymaramma <edesandraoge@gmail.com>
2023-05-06 14:07:15 -0700

    pack web

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,16 +1,27 @@
 #!/usr/bin/python3
-# Create a tar archive from web_static folder
+"""Generate .tgz file from the contents of the web_static folder"""
 
-from fabric.api import local
+from fabric import api
 from datetime import datetime
+import os
 
 
 def do_pack():
-    """ Create a tar archive from web_static folder """
-    local("mkdir -p versions")
-    date = datetime.now().strftime("%Y%m%d%H%M%S")
-    path = "versions/web_static_{}.tgz".format(date)
-    result = local("tar -cvzf {} web_static".format(path), capture=True)
-    if result.failed:
-        return None
-    return path
+    """Function to create tarball of webstatic files from the web_static
+    folder in Airbnb_v2.
+    Returns: path of .tgz file on success, None otherwise
+    """
+    with api.settings(warn_only=True):
+        isdir = os.path.isdir('versions')
+        if not isdir:
+            mkdir = api.local('mkdir versions')
+            if mkdir.failed:
+                return None
+        suffix = datetime.now().strftime('%Y%m%d%M%S')
+        path = 'versions/web_static_{}.tgz'.format(suffix)
+        tar = api.local('tar -cvzf {} web_static'.format(path))
+        if tar.failed:
+            return None
+        size = os.stat(path).st_size
+        print('web_static packed: {} -> {}Bytes'.format(path, size))
+        return path
