4acc6fd6245b4d0f344dffefc2eaf2cbdea7d280
sandymaramma <edesandraoge@gmail.com>
2023-05-06 14:04:08 -0700

    setup

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,45 +1,14 @@
 #!/usr/bin/env bash
-# configure web server for error 404 and add custom header
+# Script using bash to setup webservers for deployment of simple HTML
+# content
 
-# if nginx exist don't install
-if ! command -v nginx &> /dev/null
-then
-    sudo apt-get update
-    sudo apt-get -y install nginx
-fi
-
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
-
-sudo sh -c "echo 'Hello World!' > /data/web_static/releases/test/index.html"
+apt-get update && \
+apt-get install -y nginx && \
+mkdir -p -m=755 /data/web_static/{releases/test,shared} || exit 0
+echo 'Testing 123' > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
-chown -R ubuntu /data/
-chgrp -R ubuntu /data/
-
-sudo touch /var/www/html/404.html
-sudo sh -c "echo \"Ceci n'est pas une page\" > /var/www/html/404.html"
-SERVER_CONFIG="server {
-	listen 80 default_server;
-	listen [::]:80 default_server;
-	root /var/www/html;
-	# Add index.php to the list if you are using PHP
-	index index.html index.htm index.nginx-debian.html;
-	server_name _;
-	add_header X-Served-By $HOSTNAME;
-	location / {
-		try_files \$uri \$uri/ =404;
-	}
-	if (\$request_filename ~ redirect_me){
-			rewrite ^ https://www.facebook.com/sandra.udeike?mibextid=ZbWKwL permanent;
-	}
-	location /hbnb_static {
-		alias /data/web_static/current;
-		index index.html index.htm;
-	}
-	error_page 404 /404.html;
-	location = /404.html{
-		internal;
-	}
-}"
-sudo sh -c "echo '$SERVER_CONFIG' > /etc/nginx/sites-enabled/default"
-sudo service nginx restart
+chown -hR ubuntu:ubuntu /data/
+insert='\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;}'
+sed -i "37i $insert" /etc/nginx/sites-available/default
+service nginx restart
+exit 0
