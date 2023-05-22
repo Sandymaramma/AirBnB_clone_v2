690a75c650b87a6e386d3a32e4155a28ef2664a9
sandymaramma <edesandraoge@gmail.com>
2023-05-22 13:49:54 -0700

    hello flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-  2 """ a script that starts a Flask web application """
-  3 from flask import Flask
-  4 
-  5 app = Flask('web_flask')
-  6 
-  7 @app.route('/', strict_slashes=False)
-  8 def home():
-  9         return 'Hello HBNB!'
- 10 
- 11 if __name__ == "__main__":
- 12         app.run(host="0.0.0.0.", port=5000)
+"""Flask framework"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_world():
+    return "Hello HBNB!"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
